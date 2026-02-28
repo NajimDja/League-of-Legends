611f75ee5599e1cef62192a75d9bdc43bad51cda
Najim Djareddir <najprgm@gmail.com>
2026-02-28 16:14:35 +0100

    Update

--- src/champion.py
+++ src/champion.py
@@ -3,19 +3,23 @@ import json
 
 class ExtractChampionData:
 
+    def __init__(self):
+        self.last_version = self.get_latest_version()
+
     def get_latest_version(self):
-        versions = requests.get("https://ddragon.leagueoflegends.com/api/versions.json").json()
-        latest = versions[0]
-        print(latest)
+        url = "https://ddragon.leagueoflegends.com/api/versions.json"
+        rep = requests.get(url).json()
+        latest = rep[0]
+        return latest
 
     def get_all_champion(self):
-        url = "https://ddragon.leagueoflegends.com/cdn/16.4.1/data/fr_FR/champion.json"
+        url = f"https://ddragon.leagueoflegends.com/cdn/{self.last_version}/data/fr_FR/champion.json"
         resp = requests.get(url).json()
         print(resp)
         return resp
     
     def get_image_champion(self):
-        url = "https://ddragon.leagueoflegends.com/cdn/16.4.1/img/champion/Aatrox.png"
+        url = f"https://ddragon.leagueoflegends.com/cdn/{self.last_version}/img/champion/Aatrox.png"
         resp = requests.get(url)
         with open(r"C:\Users\najim\Documents\Projets\LeagueOfLegends\images\Aatrox.png", "wb") as file:
             file.write(resp.content)
