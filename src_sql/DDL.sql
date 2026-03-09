0e4c5de4cfe854288886e4127f6e941ca74d2092
Najim Djareddir <najprgm@gmail.com>
2026-03-09 21:20:27 +0100

    Complétion du script sql (ddl) pour les tables champions

--- src_sql/DDL.sql
+++ src_sql/DDL.sql
@@ -2,14 +2,49 @@
 
 CREATE TABLE champion (
     id      INTEGER,
-    name    TEXT,
+    name    TEXT NOT NULL,
     title   TEXT,
     lore    TEXT,
     tags    TEXT,
     partype TEXT,
     version TEXT,
+    is_latest BOOLEAN NOT NULL DEFAULT TRUE
     CONSTRAINT pk_champion
         PRIMARY KEY (id)
+);
+
+-- Schéma de la table champion_info
+
+CREATE TABLE champion_info (
+    champ_id INTEGER,
+    allytips TEXT,
+    enemytips TEXT,
+    attack	INTEGER,
+    defense	INTEGER,
+    magic	INTEGER,
+    difficulty INTEGER,
+    version TEXT,
+    is_latest BOOLEAN NOT NULL DEFAULT TRUE,
+    CONSTRAINT pk_champion_info
+        PRIMARY KEY (champ_id, version),
+    CONSTRAINT fk_champion_info_champion
+        FOREIGN KEY (champ_id) REFERENCES champion(id)
+        ON DELETE CASCADE
+);
+
+-- schéma de la table champion_passive
+
+CREATE TABLE champion_passive (
+    champ_id INTEGER,
+    name	TEXT,
+    description	TEXT,
+    version TEXT,
+    is_latest BOOLEAN NOT NULL DEFAULT TRUE,
+    CONSTRAINT pk_champion_passive
+        PRIMARY KEY (champ_id, version),
+    CONSTRAINT fk_champion_passive_champion
+        FOREIGN KEY (champ_id) REFERENCES champion(id)
+        ON DELETE CASCADE
 );
 
 -- Schéma de la table champion_spells
@@ -24,12 +59,59 @@ CREATE TABLE champion_spells (
     cost_burn    TEXT,
     cost_type    TEXT,
     maxammo      INTEGER,
-    range_burn   INTEGER,
+    range_burn   VARCHAR,
     spell_rank   INTEGER,
     version      TEXT,
+    is_latest    BOOLEAN NOT NULL DEFAULT TRUE,
     CONSTRAINT pk_champion_spells
         PRIMARY KEY (spell_id, champ_id, version),
     CONSTRAINT fk_champion_spells_champion
+        FOREIGN KEY (champ_id) REFERENCES champion(id)
+        ON DELETE CASCADE
+);
+
+-- Schéma de la table champion_stats
+
+CREATE TABLE champion_stats (
+    champ_id INTEGER,
+    hp	INTEGER,
+    mp	INTEGER,
+    movespeed	INTEGER,
+    armor	INTEGER,
+    spellblock	INTEGER,
+    attackrange	INTEGER,
+    hpregen	REAL,
+    mpregen	REAL,
+    crit	INTEGER,
+    attackdamage	INTEGER,
+    attackspeed	REAL,
+    version TEXT,
+    is_latest BOOLEAN NOT NULL DEFAULT TRUE,
+    CONSTRAINT pk_champion_stats
+        PRIMARY KEY (champ_id, version),
+    CONSTRAINT fk_champion_stats_champion
+        FOREIGN KEY (champ_id) REFERENCES champion(id)
+        ON DELETE CASCADE
+);
+
+-- Schéma de la table champion_stats_up
+
+CREATE TABLE champion_stats_up (
+    champ_id INTEGER,
+    hp_up	INTEGER,
+    mp_up	REAL,
+    armor_up	REAL,
+    spellblock_up REAL,
+    hpregen_up	REAL,
+    mpregen_up	REAL,
+    crit_up	REAL,
+    attackdamage_up	REAL,
+    attackspeed_up	REAL,
+    version TEXT,
+    is_latest BOOLEAN NOT NULL DEFAULT TRUE,
+    CONSTRAINT pk_champion_stats_up
+        PRIMARY KEY (champ_id, version),
+    CONSTRAINT fk_champion_stats_up_champion
         FOREIGN KEY (champ_id) REFERENCES champion(id)
         ON DELETE CASCADE
 );
